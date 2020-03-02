aaa8f6bb2813b824dfc88e75b1e1cc41bed17d9c
kubasikora <283418@pw.edu.pl>
2020-03-02 19:44:02 +0100

    working with pixel access from mat container]

--- DisplayImage.cxx
+++ DisplayImage.cxx
@@ -1,6 +1,15 @@
 #include<iostream>
 #include<opencv2/opencv.hpp>
 
+void printImage(const cv::Mat& image){
+    image.forEach<cv::Vec3b>([](const cv::Vec3b& pixel, const int position[]){
+        std::cout << "BGR values of pixel (" << position[0] << "," << position[1] << ")" << std::endl;
+        std::cout << "B: " << static_cast<int>(pixel[0]) << std::endl;
+        std::cout << "G: " << static_cast<int>(pixel[1]) << std::endl;
+        std::cout << "R: " << static_cast<int>(pixel[2]) << std::endl;
+    });
+}
+
 int main(int argc, char** argv){
     if(argc != 2){
         std::cout << "Usage: DisplayImage <imagePath>" << std::endl;
@@ -13,6 +22,12 @@ int main(int argc, char** argv){
         std::cout << "Empty image" << std::endl;
 	    return -1;
     }
+
+    std::cout << "BGR values of first pixel (0,0):" << std::endl;
+    std::cout << "B: " << static_cast<int>(image.at<cv::Vec3b>(0,0)[0]) << std::endl;
+    std::cout << "G: " << static_cast<int>(image.at<cv::Vec3b>(0,0)[1]) << std::endl;
+    std::cout << "R: " << static_cast<int>(image.at<cv::Vec3b>(0,0)[2]) << std::endl;
+
 
     cv::namedWindow("Display image", cv::WINDOW_AUTOSIZE);
     cv::imshow("Display image", image);
